84ef785d2a264355cbd91647c5388bc685d66cc0
kirillka.k@gmail.com <108414175+Kiryaoo@users.noreply.github.com>
2025-05-17 12:28:04 +0300

    Added integration tests

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -19,14 +19,36 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
-	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
-	if err != nil {
-		t.Error(err)
+	const requestsCount = 10
+	seen := make(map[string]bool)
+
+	for i := 0; i < requestsCount; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			t.Fatalf("Request %d failed: %v", i+1, err)
+		}
+		defer resp.Body.Close()
+
+		server := resp.Header.Get("lb-from")
+		if server == "" {
+			t.Errorf("Missing lb-from header in response %d", i+1)
+		} else {
+			t.Logf("Response %d from [%s]", i+1, server)
+			seen[server] = true
+		}
 	}
-	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	if len(seen) < 2 {
+		t.Errorf("Expected responses from at least 2 different servers, but got: %v", seen)
+	}
 }
 
 func BenchmarkBalancer(b *testing.B) {
-	// TODO: Реалізуйте інтеграційний бенчмарк для балансувальникка.
+	for i := 0; i < b.N; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			b.Fatalf("Request failed: %v", err)
+		}
+		resp.Body.Close()
+	}
 }
